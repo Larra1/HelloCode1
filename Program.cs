a36bb01708c2082692e60d545e066220cfed062b
Ivan Odobesko <odobesko.ivan@gmail.com>
2023-05-22 16:01:37 +0300

    Second commit

--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("2Hello, World!");
+﻿Console.WriteLine("2Hello, World!");
+Console.WriteLine("Привет, братишка!");
+
 
