f0ab2312be7fd3c052d264eb3cc671a3619bfd70
Ivan Odobesko <odobesko.ivan@gmail.com>
2023-05-22 22:08:10 +0300

    Eighth commit-if-else-masha

---
+++ 5_if-else/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine ("Введите имя пользователя");
+string username=Console.ReadLine();
+if (username.ToLower()=="маша") //добавил оператор ToLower()=="маша" для избежания ошибок при вводе Маша, маша, МАШа итд
+{
+    Console.WriteLine("Ура, это Маша");
+}
+else
+{
+    Console.WriteLine("Привет,");
+    Console.WriteLine(username);
+}
